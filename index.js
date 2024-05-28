11517166cefada6b9c6314d39ef8891ace58e12e
akukamil <chamilioner_kid@mail.ru>
2024-05-28 12:07:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5731,7 +5731,7 @@ async function init_game_env(lang) {
 	objects.id_name.set2(my_data.name,150);
 	
 	//номер комнаты в зависимости от рейтинга игрока
-	const rooms_bins=[0,1392,1463,9999];
+	const rooms_bins=[0,1395,1483,9999];
 	for (let i=1;i<rooms_bins.length;i++){
 		const f=rooms_bins[i-1];
 		const t=rooms_bins[i];		
